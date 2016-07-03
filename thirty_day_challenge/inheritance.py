56d60e74237b716d9f82d5059d3e27a31ef739ba
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-07-03 15:46:58 -0700

    Class inheritance

---
+++ thirty_day_challenge/inheritance.py
@@ -0,0 +1,50 @@
+from statistics import mean
+
+class Person:
+    def __init__(self, firstName, lastName, idNumber):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.idNumber = idNumber
+    def printPerson(self):
+        print("Name:", self.lastName + ",", self.firstName)
+        print("ID:", self.idNumber)
+
+
+class Student(Person):
+
+    def __init__(self, firstName, lastName, idNumber, scores):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.idNumber = idNumber
+        self.scores = scores
+
+    def calculate(self):
+        avg = mean(self.scores)
+        if avg >= 90 and avg <= 100:
+            grade = 'O'
+        elif avg >= 80 and avg < 90:
+            grade = 'E'
+        elif avg >= 70 and avg < 80:
+            grade = 'A'
+        elif avg >= 55 and avg < 70:
+            grade = 'P'
+        elif avg >= 40 and avg < 55:
+            grade = 'D'
+        else:
+            grade = 'T'
+        return grade
+
+
+
+
+
+
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
